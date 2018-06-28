4093871642bbb068de1beac1a08d5f2423926b92
Marlon Klagenberg Conceição <mkconceicao@stefanini.com>
2018-06-28 10:06:52 -0300

    :tada: initial commit

---
+++ admin/src/main/java/br/gov/badesc/admin/AdminApplication.java
@@ -0,0 +1,14 @@
+package br.gov.badesc.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
